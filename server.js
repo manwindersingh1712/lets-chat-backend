d9cefe133c32a7ab21a515b6a65b2cba6788fa67
manwinder <manwindersingh1712@gmail.com>
2022-01-15 02:09:03 +0530

    initial setup server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+
+require("dotenv").config();
+
+const port = process.env.PORT || 8000;
+
+// check working server
+app.get("/", (req, res, next) => {
+  return res.send({ message: "working fine!!" });
+});
+
+io.on("connection", (socket) => {
+  console.log("connected to socket");
+});
+
+server.listen(port, () => {
+  console.log(`Server listening to ${port}`);
+});
