3febd94270eff4c065f4f1a6a593d77591231375
manwinder <manwindersingh1712@gmail.com>
2022-01-17 00:39:07 +0530

    updated the cors policy

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const authRouter = require("./routes/auth");
 const app = express();
 const server = http.createServer(app);
 const io = require("socket.io")(server);
+const cors = require("cors");
 
 const mongoose = require("mongoose");
 
 require("dotenv").config();
 
 const port = process.env.PORT;
-
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
