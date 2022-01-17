67f6175c17fb978cd4726efe0d79944083c8f199
manwinder <manwindersingh1712@gmail.com>
2022-01-17 23:07:13 +0530

    schema updated

--- models/message.js
+++ models/message.js
@@ -1,23 +1,32 @@
 const Schema = require("mongoose").Schema;
 const mongoose = require("mongoose");
 
-const MessageSchema = new Schema({
-  roomId: {
-    type: Schema.Types.ObjectId,
-    ref: "Rooms",
+const MessageSchema = new Schema(
+  {
+    _id: {
+      type: String,
+      required: true,
+    },
+    roomId: {
+      type: Schema.Types.ObjectId,
+      ref: "Rooms",
+    },
+    from: {
+      type: Schema.Types.ObjectId,
+      ref: "Users",
+    },
+    msg: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: String,
+      default: Date.now().toString(),
+    },
   },
-  from: {
-    type: Schema.Types.ObjectId,
-    ref: "Users",
-  },
-  msg: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: String,
-    default: Date.now().toString(),
-  },
-});
+  {
+    _id: false,
+  }
+);
 
 module.exports = mongoose.model("Messages", MessageSchema);
